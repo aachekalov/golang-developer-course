75fa9ca1cb0813b6c45c4d4fbdb8a682cef24fed
Алексей Чекалов <achekalolv@atsaero.ru>
2023-11-19 00:22:20 +0400

    03 step 1 task 3

--- 03-basic-concepts/main.go
+++ 03-basic-concepts/main.go
@@ -9,4 +9,6 @@ func main() {
 	fmt.Printf("Max int32: %v Overflow: %v\n", step1.Sum(1), step1.Sum(2))
 
 	fmt.Printf("Distance between Yerevan and Tolyatti: %v km\n", step1.GetDistanceFromLatLonInKm(40.1534924, 44.4061668, 53.5219693,49.3215277))
+
+	fmt.Printf("Is the same location: %v\n", step1.IsTheSameLocation(40.1534924, 44.4061668, 40.1534944, 44.4061688))
 }
---
+++ 03-basic-concepts/step1/3.go
@@ -0,0 +1,11 @@
+package step1
+
+import (
+	"math"
+)
+
+const eps = 0.00001
+
+func IsTheSameLocation(lat1, lon1, lat2, lon2 float32) bool {
+	return math.Abs(float64(lat1 - lat2)) < eps && math.Abs(float64(lon1 - lon2)) < eps
+}
